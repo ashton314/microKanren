88aad98d473bea399fbc7a0313c0720e2bfc4052
Ashton Wiersdorf <ashton.wiersdorf@pobox.com>
2021-03-20 22:44:09 -0600

    Finished working on source code from the paper

--- kanren.rkt
+++ kanren.rkt
@@ -1,7 +1,13 @@
 #lang racket/base
 (provide (all-defined-out))
 
-;;; Taken from μKanren by Hemann and Friedman
+;; Taken from the μKanren paper by Hemann and Friedman
+;; Resources:
+;;  - http://minikanren.org/
+;;  - http://webyrd.net/scheme-2013/papers/HemannMuKanren2013.pdf
+
+;;;-------------------------
+;;; µKanren Core
 
 ;; Variables
 (define (var c) (vector c))
@@ -76,3 +82,91 @@
   (if (null? lst)
       #f
       (if (? (caar lst)) (car lst) (assp ? (cdr lst)))))
+
+;;;-------------------------
+;;; User-level Functionality
+
+;; "Snooze": automate the inverse-η-delay
+(define-syntax Zzz
+  (syntax-rules ()
+    [(_ g) (λ (s/c) (λ () (g s/c)))]))
+
+;; Variadic conj and disj
+(define-syntax conj+
+  (syntax-rules ()
+    [(_ g) (Zzz g)]
+    [(_ g0 g ...) (conj (Zzz g0) (conj+ g ...))]))
+
+(define-syntax disj+
+  (syntax-rules ()
+    [(_ g) (Zzz g)]
+    [(_ g0 g ...) (disj (Zzz g0) (disj+ g ...))]))
+
+(define-syntax conde
+  (syntax-rules ()
+    [(_ (g0 g ...) ...)
+     (disj+ (conj+ g0 g ...) ...)]))
+
+(define-syntax fresh
+  (syntax-rules ()
+    [(_ () g0 g ...)
+     (conj+ g0 g ...)]
+    [(_ (x0 x ...) g0 g ...)
+     (call/fresh (λ (x0) (fresh (x ...) g0 g ...)))]))
+
+;; Stream->List functionality
+(define (pull $) (if (procedure? $) (pull ($)) $))
+
+(define (take-all $)
+  (let ([$ (pull $)])
+    (if (null? $) '() (cons (car $) (take-all (cdr $))))))
+
+(define (take n $)
+  (if (zero? n) '()
+      (let ([$ (pull $)])
+        (cond
+          [(null? $) '()]
+          [else (cons (car $) (take (- n 1) (cdr $)))]))))
+
+;; Reification utilities
+(define (mK-reify s/c*)
+  (map reify-state/1st-var s/c*))
+
+(define (reify-state/1st-var s/c)
+  (let ([v (walk* (var 0) (car s/c))])
+    (walk* v (reify-s v '()))))
+
+(define (reify-s v s)
+  (let ([v (walk v s)])
+    (cond
+      [(var? v)
+       (let ([n (reify-name (length s))])
+         (cons `(,v . ,n) s))]
+      [(pair? v) (reify-s (cdr v) (reify-s (car v) s))]
+      [else s])))
+
+(define (reify-name n)
+  (string->symbol (string-append "_" "." (number->string n))))
+
+(define (walk* v s)
+  (let ([v (walk v s)])
+    (cond
+      [(var? v) v]
+      [(pair? v) (cons (walk* (car v) s) (walk* (cdr v) s))]
+      [else v])))
+
+;; Host interface
+(define empty-state '(() . 0))
+(define (call/empty-state g) (g empty-state))
+
+(define-syntax run
+  (syntax-rules ()
+    [(_ n (x ...) g0 g ...)
+     (mK-reify (take n (call/empty-state
+                        (fresh (x ...) g0 g ...))))]))
+
+(define-syntax run*
+  (syntax-rules ()
+    [(_ (x ...) g0 g ...)
+     (mK-reify (take-all (call/empty-state
+                          (fresh (x ...) g0 g ...))))]))
